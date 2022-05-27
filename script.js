88c1956bf88c9b85390d84c9bb99cd02622b0bb7
kiruidavid <david.wendot@gmail.com>
2022-05-27 19:15:59 +0300

    Added functionality to the calculator

--- script.js
+++ script.js
@@ -1,30 +1,109 @@
-let calculator = document.querySelector('.calculator') 
-let keys = document.querySelector('.calcKeys') 
+const calculator = {
+    displayValue: '0' , 
+    firstOperand: null, 
+    waitingForSecondOperand: false, 
+    operator: null,
+} 
+function updateDisplay(){
+    const display = document.querySelector('.calcDisplay') 
+    display.value = calculator.displayValue
+} 
+updateDisplay() 
+const keys = document.querySelector('.calcKeys') 
+keys.addEventListener('click', (e) => {
+    if(!e.target.matches('button')){
+        return;
+    } 
+    switch (e.target.value) {
+        case '+': 
+        case '-':  
+        case '*': 
+        case '/':  
+        case '=': 
+          handleOperator(e.target.value);
+          break; 
+          case '.': 
+          inputDecimal(e.target.value); 
+          break; 
+          case 'all-clear': 
+          resetCalc(); 
+          break; 
+          default: 
+          if(Number.isInteger(parseFloat(e.target.value))){
+              inputDigit(e.target.value)
+          }
 
-keys.addEventListener('click', e => {
-    if(e.target.matches('button')){
-        let key = e.target 
-        let action = key.dataset.action 
 
-        if(!action) {
-            console.log('number key!')
-        } 
-        if (
-            action == 'add' || 
-            action == 'subtract' || 
-            action == 'multiply' || 
-            action == 'divide') {
-                console.log('operator key')
-        } 
-        if(action === 'decimal'){
-            console.log('decimal key')
-        } 
-        if(action === 'clear'){
-            console.log('clear key')
-        } 
-        if(action === 'decimal'){
-            console.log('equal key')
-        }
-        
+    } 
+    updateDisplay()
+    
+    
+}) 
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator 
+    if(waitingForSecondOperand == true ){
+        calculator.displayValue = digit 
+        calculator.waitingForSecondOperand = false
+    } else {
+       calculator.displayValue = displayValue == '0' ? digit : displayValue + digit 
+    } 
+    console.log(calculator)
+} 
+
+function inputDecimal(dot) { 
+    if (calculator.waitingForSecondOperand == true){ 
+        calculator.displayValue = '0.' 
+        calculator.waitingForSecondOperand = false
+        return
+
     }
-})
+    if(!calculator.displayValue.includes(dot)){ 
+        calculator.displayValue += dot
+
+    }
+} 
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator 
+    const inputValue = parseFloat(displayValue) 
+    if (operator && calculator.waitingForSecondOperand){
+       calculator.operator = nextOperator 
+       console.log(calculator) 
+       return
+    }
+
+    if (firstOperand === null && !isNaN(inputValue)) {
+        calculator.firstOperand = inputValue
+    } else if (operator) { 
+        const result = calculate(firstOperand, inputValue, operator) 
+        calculator.displayValue = `${parseFloat(result.toFixed(1))}` 
+        calculator.firstOperand = result
+
+    }
+    calculator.waitingForSecondOperand = true 
+    calculator.operator = nextOperator 
+    console.log(calculator)
+}  
+
+function calculate(firstOperand, secondOperand, operator){
+    if(operator == '+'){
+       return firstOperand + secondOperand;
+    } else if (operator == '-'){
+       return firstOperand - secondOperand
+    } else if(operator == "*"){ 
+       return firstOperand * secondOperand
+
+    } else if(operator == "/"){ 
+        return firstOperand / secondOperand
+
+    } 
+    
+        return secondOperand
+    
+} 
+function resetCalc(){
+    calculator.displayValue = 0 
+    calculator.firstOperand = null 
+    calculator.waitingForSecondOperand = false 
+    calculator.operator = null 
+    console.log(calculator)
+}
