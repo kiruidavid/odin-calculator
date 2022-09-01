05b2bad8a9cda34d77708c4d2cc1ecabd57eb42f
kiruidavid <david.wendot@gmail.com>
2022-09-01 19:13:30 +0300

    Refactored code to pass freecodecamp challenge

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const calculator = {
-    displayValue: '0' , 
+    displayValue: '0', 
     firstOperand: null, 
     waitingForSecondOperand: false, 
-    operator: null,
+    operator: null
 } 
 function updateDisplay(){
-    const display = document.querySelector('.calcDisplay') 
+    let display = document.getElementById('display') 
     display.value = calculator.displayValue
 } 
 updateDisplay() 
 const keys = document.querySelector('.calcKeys') 
-keys.addEventListener('click', (e) => {
+keys.addEventListener('click' , (e) => {
     if(!e.target.matches('button')){
         return;
     } 
-    switch (e.target.value) {
+    switch (e.target.value){
         case '+': 
-        case '-':  
+        case '-': 
         case '*': 
-        case '/':  
+        case '/': 
         case '=': 
-          handleOperator(e.target.value);
-          break; 
+          handleOperator(e.target.value); 
+          break;  
           case '.': 
           inputDecimal(e.target.value); 
           break; 
@@ -30,75 +30,65 @@ keys.addEventListener('click', (e) => {
           break; 
           default: 
           if(Number.isInteger(parseFloat(e.target.value))){
-              inputDigit(e.target.value)
+            inputDigit(e.target.value)
           }
-
-
+         
     } 
     updateDisplay()
-    
-    
 }) 
-function inputDigit(digit) {
-    const { displayValue, waitingForSecondOperand } = calculator 
-    if(waitingForSecondOperand == true ){
+function inputDigit(digit){
+    const {displayValue, waitingForSecondOperand} = calculator 
+    if(waitingForSecondOperand == true){
         calculator.displayValue = digit 
         calculator.waitingForSecondOperand = false
     } else {
-       calculator.displayValue = displayValue == '0' ? digit : displayValue + digit 
+        calculator.displayValue = displayValue == '0' ? digit : displayValue + digit
     } 
     console.log(calculator)
 } 
-
-function inputDecimal(dot) { 
-    if (calculator.waitingForSecondOperand == true){ 
-        calculator.displayValue = '0.' 
-        calculator.waitingForSecondOperand = false
-        return
-
-    }
-    if(!calculator.displayValue.includes(dot)){ 
+function inputDecimal(dot){
+    if(calculator.waitingForSecondOperand == true){
+      calculator.displayValue = '0.' 
+      calculator.waitingForSecondOperand = false 
+      return
+    } 
+    if(!calculator.displayValue.includes(dot)){
         calculator.displayValue += dot
-
     }
 } 
-function handleOperator(nextOperator) {
-    const { firstOperand, displayValue, operator } = calculator 
-    const inputValue = parseFloat(displayValue) 
-    if (operator && calculator.waitingForSecondOperand){
-       calculator.operator = nextOperator 
-       console.log(calculator) 
-       return
-    }
-
-    if (firstOperand === null && !isNaN(inputValue)) {
-        calculator.firstOperand = inputValue
-    } else if (operator) { 
-        const result = calculate(firstOperand, inputValue, operator) 
-        calculator.displayValue = `${parseFloat(result.toFixed(1))}` 
-        calculator.firstOperand = result
-
-    }
-    calculator.waitingForSecondOperand = true 
+function handleOperator(nextOperator){
+   const {firstOperand, displayValue, operator} = calculator 
+   const inputValue = parseFloat(displayValue) 
+   if(operator && calculator.waitingForSecondOperand){
     calculator.operator = nextOperator 
-    console.log(calculator)
-}  
-
+    console.log(calculator) 
+    return
+   } 
+   if (firstOperand === null && !isNaN(inputValue)){
+    calculator.firstOperand = inputValue
+   } else if (operator){
+    const result = calculate(firstOperand, inputValue, operator) 
+    calculator.displayValue = `${parseFloat(result.toFixed(4))}` 
+    calculator.firstOperand = result
+   } 
+   calculator.waitingForSecondOperand = true 
+   calculator.operator = nextOperator 
+   console.log(calculator)
+} 
 function calculate(firstOperand, secondOperand, operator){
     if(operator == '+'){
-       return firstOperand + secondOperand;
-    } else if (operator == '-'){
-       return firstOperand - secondOperand
-    } else if(operator == "*"){ 
-       return firstOperand * secondOperand
+        return firstOperand + secondOperand
+    } else if (operator == '-'){ 
+        return firstOperand - secondOperand
 
-    } else if(operator == "/"){ 
+    } else if(operator == '*'){ 
+        return firstOperand * secondOperand
+
+    } else if(operator == '/'){ 
         return firstOperand / secondOperand
 
     } 
-    
-        return secondOperand
-    
+    return secondOperand
 } 
 function resetCalc(){
     calculator.displayValue = 0 
